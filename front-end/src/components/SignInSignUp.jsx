b401bc7720c63ba54dab2507c606a632c9b557d5
Shazan Suria <shazansuria@gmail.com>
2025-06-24 10:18:11 +0530

    Resolve merge conflicts

--- front-end/src/components/SignInSignUp.jsx
+++ front-end/src/components/SignInSignUp.jsx
@@ -100,7 +100,7 @@ const SignInSignUp = () => {
 
       const result = await response.json();
       console.log(result); // Add this line
-      if (result.success && result.email === import.meta.env.VITE_ADMIN_EMAIL && password === import.meta.env.VITE_ADMIN_PASSWORD) {
+      if (result.success && result.email === import.meta.env.VITE_ADMIN_EMAIL && result.password === import.meta.env.VITE_ADMIN_PASSWORD) {
         localStorage.clear();
         localStorage.setItem("admin", JSON.stringify(result));
         window.location.href = "/dashboard";
